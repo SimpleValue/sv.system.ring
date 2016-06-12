8010d56a10880324f17dbc6335599b9272360a15
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-12 14:41:29 +0200

    Extracted sv.system.ring from sv/system.

---
+++ README.md
@@ -0,0 +1,12 @@
+# system.ring
+
+Components and helper functions to use
+[Ring](https://github.com/ring-clojure/ring) in a sv/system-based
+application.
+
+## License
+
+Copyright © 2016 Max Weber (SimpleValue)
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject sv/system.ring "0.1.0-SNAPSHOT"
+  :description "Components and helper functions to use Ring in a
+                sv/system-based application."
+  :url "https://github.com/SimpleValue/sv.system.ring"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
